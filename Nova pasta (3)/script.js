b45f562650523bea8cfdd4f4065c0ebec97a17c7
sosh687 <sophiamoritadantas7@gmail.com>
2025-04-15 18:50:54 -0300

    Update script.js

--- Nova pasta (3)/script.js
+++ Nova pasta (3)/script.js
@@ -1,9 +1,82 @@
-function validateForm() {
-    var usar = document.getElementsById("username").value;
-    var pass = document.getElementById("password").value;
-    if (user === "" || pass === "") {
-       alert("Preencha todos os campos.");
-       return falses;
+body{
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
 }
- return true;
+
+.login-container {
+    width: 300px;
+    margin: 100px;
+    padding: 30px;
+    background: #fff;
+    box-shadow:  0 0 10px rgba(0, 0, 0, 1);
+}
+
+.input, button {
+    display: block;
+    width: 100px;
+    padding: 10px;
+    margin-bottom: 15px;
+}
+
+button{
+    background: #5cb85c;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+
+.button:hover {
+    background: #4cae4c;
+}
+
+.site-header {
+    background-color: #004080;
+    color: white;
+    padding: 20px;
+    text-align: center;
+}
+
+.site-header nav ul {
+    list-style: none;
+    padding: 0;
+}
+
+.site-header nav ul {
+    display: inline;
+    margin: 0 10px;
+}
+
+.site-header nav ul {
+    color: white;
+    text-decoration: none;
+}
+
+.content {
+    padding: 20px;
+}
+
+.site-fooder {
+    background-color: #222;
+    color: white;
+    text-align: center;
+    padding: 15px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+
+.login-status {
+    text-align: right;
+    margin-right: 20px;
+    color: #fff;
+}
+
+#ex31 {
+    font-size: 14px;
+    color: #4cae4c;
+    text-align: center;
+    margin-top: 10px;
 }
